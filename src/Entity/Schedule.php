09b679f7d780547b7bf857edaf00d095235d47b9
Michael Dekker <git@michaeldekker.nl>
2019-12-10 11:37:42 +0100

    Remove collation Schedule

--- src/Entity/Schedule.php
+++ src/Entity/Schedule.php
@@ -8,7 +8,6 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Table(
  *   name="task_schedules",
- *   options={"collate"="utf8_swedish_ci"},
  *   indexes={
  *     @ORM\Index(name="active_idx", columns={"active"})
  *   }
